3c4cdf19cc7a6a313bdeadce79c1759a51401628
ChampFu <champfu@mcdex.io>
2021-09-24 19:12:37 +0800

    add position

--- src/core.ts
+++ src/core.ts
@@ -1,51 +1,30 @@
-import {BigInt, BigDecimal, ethereum, log, Address} from "@graphprotocol/graph-ts"
-import {LiquidityPool, PriceBucket, MarkPrice} from "../generated/schema"
+import {LiquidityPool, MarkPrice} from "../generated/schema"
 import {
     Trade as TradeEvent,
+    UpdatePrice as UpdatePriceEvent,
+    Redeem as RedeemEvent,
+    Stake as StakeEvent
 } from '../generated/templates/LiquidityPool/LiquidityPool'
 
 import {
-    BI_18,
-    ADDRESS_ZERO,
-    fetchUser,
-    fetchTradeAccount,
-    convertToDecimal,
     AbsBigDecimal,
-    fetchMiningInfo,
-    fetchLiquidityPool,
-    isReferrerInWhiteList,
+    BI_18,
+    convertToDecimal,
+    fetchTradeAccount,
+    fetchUser,
     getTokenPrice,
-    ZERO_BD, fetchMarkPrice,
+    splitCloseAmount,
+    splitOpenAmount,
+    ZERO_BD
 } from "./utils"
-import {MCB_ADDRESS} from "./const"
 import {getMCBPrice} from "./uniswap"
-import {updateTradeMiningDayData} from "./dataUpdate";
 
 export function handleTrade(event: TradeEvent): void {
-    let miningInfo = fetchMiningInfo()
-    // mining budget reach
-    if ((miningInfo.budget <= miningInfo.minedBudget) || (miningInfo.rebateRate == ZERO_BD)) {
-        return
-    }
     let poolAddr = event.address.toHexString()
     let liquidityPool = LiquidityPool.load(poolAddr)
     if (liquidityPool == null) {
         return
     }
-
-    // check pool in mining pool list
-    let isExist = false
-    let pools = miningInfo.pools
-    for (let index = 0; index < pools.length; index++) {
-        if (poolAddr == pools[index]) {
-            isExist = true
-            break
-        }
-    }
-    if (!isExist) {
-        return
-    }
-
     let user = fetchUser(event.params.trader)
     // user account in each pool
     let account = fetchTradeAccount(user, liquidityPool as LiquidityPool)
@@ -57,53 +36,47 @@ export function handleTrade(event: TradeEvent): void {
 
     let tokenPrice = getTokenPrice(liquidityPool.collateralAddress, event.block.timestamp)
 
-    let mcbPrice = getMCBPrice()
-
     let feeUSD = fee.times(tokenPrice)
     let volumeUSD = volume.times(tokenPrice)
-    let rebateValue = fee.times(miningInfo.rebateRate).times(tokenPrice).div(mcbPrice)
-
-
-    // update mined budget
-    let minedBudget = miningInfo.minedBudget + rebateValue
-    if (minedBudget > miningInfo.budget) {
-        rebateValue -= (minedBudget - miningInfo.budget)
-    }
-    miningInfo.minedBudget += rebateValue
-    miningInfo.save()
-
-    // update trade mining day data
-    updateTradeMiningDayData(poolAddr, event.block.timestamp, rebateValue, mcbPrice)
 
     // update user earned MCB
     user.totalFee += feeUSD
-    user.totalEarnMCB += rebateValue
-    user.unPaidMCB += rebateValue
-    user.save()
 
-
-    // update account trade info 
+    // update account trade info
     account.tradeVolume += volume
     account.tradeVolumeUSD += volumeUSD
     account.totalFee += fee
     account.totalFeeUSD += feeUSD
-    account.earnMCB += rebateValue
     account.save()
+
+    let close = splitCloseAmount(user.position, position)
+    let open = splitOpenAmount(user.position, position)
+
+    // close position
+    if (close != ZERO_BD) {
+        user.position = user.position.plus(close)
+    }
+
+    // open position
+    if (open != ZERO_BD) {
+        user.position = user.position.plus(open)
+    }
+    user.save()
 }
 
-export function handleRedeem(event: Redeem): void {
+export function handleRedeem(event: RedeemEvent): void {
     let user = fetchUser(event.params.account)
     user.stackMCB -= event.params.redeemed
     user.save()
 }
 
-export function handleStake(event: Stake): void {
+export function handleStake(event: StakeEvent): void {
     let user = fetchUser(event.params.account)
     user.stackMCB = event.params.totalStaked
     user.save()
 }
 
-export function handleUpdatePrice(event: UpdatePrice): void {
+export function handleUpdatePrice(event: UpdatePriceEvent): void {
     let id = event.address.toHexString()
       .connect('-')
       .connect(event.params.perpetualIndex.toString()
