8e9e8aad6c61201abf25a37829fc1c3f4fb4179b
renpu <renpu@boo.chat>
2021-11-10 10:28:32 +0800

    update

--- src/utils.ts
+++ src/utils.ts
@@ -52,6 +52,10 @@ export function fetchMarginAccount(user: User, pool: Address, perpetualIndex: Bi
     marginAccount.lpFee = ZERO_BD
     marginAccount.operatorFee = ZERO_BD
     marginAccount.vaultFee = ZERO_BD
+    marginAccount.totalFeeFactor = ZERO_BD
+    marginAccount.lpFeeFactor = ZERO_BD
+    marginAccount.operatorFeeFactor = ZERO_BD
+    marginAccount.vaultFeeFactor = ZERO_BD
     marginAccount.referralRebate = ZERO_BD
     marginAccount.save()
   }
